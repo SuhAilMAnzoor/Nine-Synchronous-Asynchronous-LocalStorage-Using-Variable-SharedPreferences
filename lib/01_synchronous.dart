a41fbcffbb56c8ce526b1b05559b9e1cee60ad32
SuhailManzoor <sohailmanzoorsario@gmail.com>
2025-06-05 16:54:11 +0500

    Synchronous Function is Execute complete code in once and don't wait for any I-O Or Another Operation to be peformed like API

---
+++ lib/01_synchronous.dart
@@ -0,0 +1,19 @@
+// Synchronous Programming
+void main(){
+  num number1  = 34;
+  num number2  = 56;
+  num result = number1 + number2;
+  print(result);
+}
+// line by line works not wait for Any Operation or API
+// or I/O
+
+// Another Example of Synchronous
+
+// void main(){
+//   print("Object");
+//   print("Object");
+//   print("Object");
+//   print("Object");
+//
+// }
